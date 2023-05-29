9745c5c25335fea124fed36e9fa5b0e8f3b5b748
Kiri Kiri <killian.podhajsky@gmail.com>
2023-05-29 13:33:15 -0700

    routes

---
+++ models/Thoughts.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create a thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+      min_length: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+
+    reactions: [reactionSchema]
+    
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
